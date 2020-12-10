bff012d64413c05d7acd5e7f71c203334555190e
andreaavr <alu0101400655@ull.edu.es>
2020-12-10 18:47:16 +0000

    funciones-practica9

--- ejercicio1/find_first_of.cc
+++ ejercicio1/find_first_of.cc
@@ -1,13 +1,31 @@
+
 #include <iostream>
-#include <string>
 #include <cstring>
-#include "find_first_of.h"
 using namespace std;
 
+// Funcion que muestra por pantalla el mensaje de error
+void usage (int argc, char *argv[]){
+  if (argc == 2 && strcmp(argv[1], "--help") == 0){
+    cout << "Este programa necesita que al compilarlo introduzca una palabra y un caracter para ver en que posicion de la palabra esta el caracter" << endl;
+    exit (EXIT_SUCCESS);
+  }
+  if (argc == 1){
+    cout << "Pruebe 'ejecutable -- help' para mas informacion " << endl;
+    exit(EXIT_SUCCESS);
+  }
+  }
 
-int main(int argc, char *argv[]){
-  usage (argc, argv);
-  string word =argv[1];
-  string character_to_find = argv[2];
-  cout << PositionOfCharacter(word, character_to_find) << endl;
+// Funcion que va comparando el caracter dado con los caracteres de la palabra introducida
+int PositionOfCharacter (string word, string character_to_find){
+  int cout;
+  for (auto &character: word){
+    if(character_to_find.find(character) == string::npos){
+      cout++ ;
+      continue;
+    }
+    else{
+      return cout;
+    }
+  }
+  return -1;
 }

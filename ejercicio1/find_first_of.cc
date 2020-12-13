75eb38a1d328f31d3ab33bb21ff291121e73c404
andreaavr <alu0101400655@ull.edu.es>
2020-12-13 13:45:17 +0000

    funcione-practica9

--- ejercicio1/find_first_of.cc
+++ ejercicio1/find_first_of.cc
@@ -1,8 +1,24 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author Andrea Venazco Rodríguez
+ * @date 8 Dic 2020
+ * 
+ * Este programa toma como parámetro por la línea de comando una cadena y un carácter
+ *
+ * Después busca la posición de la cadena en la que está el carácter
+ *
+ * El programa imprimirá la posicion del carácter y sino se encuentra dicho carácter mostrará un -1
+ */
+
 #include <iostream>
 #include <cstring>
+
 using namespace std;
 
-// Funcion que muestra por pantalla el mensaje de error
 void usage (int argc, char *argv[]){
 	if (argc == 2 && strcmp(argv[1], "--help") == 0){
     	cout << "Este programa necesita que al compilarlo introduzca una palabra y un caracter para ver en que posicion de la palabra esta el caracter" << endl;
@@ -14,7 +30,6 @@ void usage (int argc, char *argv[]){
   	}
 }
 
-// Funcion que va comparando el caracter dado con los caracteres de la palabra introducida
 int PositionOfCharacter (string word, string character_to_find){
 	int cout;
 	for (auto &character: word){
--- ejercicio1/find_first_of_main.cc
+++ ejercicio1/find_first_of_main.cc
@@ -1,7 +1,26 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author Andrea Venazco Rodríguez
+ * @date 8 Dic 2020
+ * 
+ * Este programa toma como parámetro por la línea de comando una cadena y un car
+ácter
+ *
+ * Después busca la posición de la cadena en la que está el carácter
+ *
+ * El programa imprimirá la posicion del carácter y sino se encuentra dicho cará
+cter mostrará un -1
+ */
+
 #include <iostream>
 #include <string>
 #include <cstring>
 #include "find_first_of.h"
+
 using namespace std;
 
 int main(int argc, char *argv[]){
